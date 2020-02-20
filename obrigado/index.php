82c41b175ec7c256a6602eb9e60e497f5c8825d9
dudouz <ddz.iotti@gmail.com>
2020-02-20 19:07:12 -0300

    google analytics

--- obrigado/index.php
+++ obrigado/index.php
@@ -42,6 +42,16 @@ mail($para, $assunto, $mensagem, $headers);  //função que faz o envio do email
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-68518108-32"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-68518108-32');
+</script>
+
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
